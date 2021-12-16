b37a8ba8b6ccb3b97fd55f74694de9e866cc21b5
Darya <d.byk@list.ru>
2021-12-16 13:25:07 +0300

    Добавьте файлы проекта.

---
+++ Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Message
+    {
+        public static void WriteError(string err)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("ERR: ");
+            Console.ResetColor();
+            Console.WriteLine(err);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //пока для проверки
+            foreach (var str in args)
+                Console.Write(str + ' ');
+            if (args.Length < 3) Message.WriteError("There must be more than 3 arguments");
+            else if (args.Length % 2 == 0) Message.WriteError("The number of arguments must be odd");
+            else if (IsCorrect(args)) ;
+            else WriteExamples();
+
+        }        
+        
+    }
+}
