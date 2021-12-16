00a4fb3843b3be695b2dc153124ee9c6f3b91189
Darya <d.byk@list.ru>
2021-12-16 13:33:42 +0300

    Начальные проверки

--- GameRules.cs
+++ GameRules.cs
@@ -9,10 +9,11 @@ namespace Game
     internal class GameRules
     {
         static string[] GameElements = { "rock", "paper", "scissors", "lizard", "Spock" };
+        static string[] Numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         //rock paper scissors lizard Spock
         public static void WriteExamples()
         {
-            Message.WriteError("Wrong arguments");
+            Message.WriteError("Arguments must match the pattern and not be repeated");
             Console.WriteLine("Try this:");
             Console.WriteLine("rock paper scissors");
             Console.WriteLine("rock paper scissors lizard Spock");
@@ -21,7 +22,9 @@ namespace Game
 
         public static bool IsCorrect(string[] args)
         {
-            foreach(var str in args) if (GameElements.Contains())
+            foreach (var str in args) 
+                if (GameElements.Contains(str)) continue;
+                else return false;
             return true;
         }
     }
--- Program.cs
+++ Program.cs
@@ -13,10 +13,11 @@ namespace Game
             //пока для проверки
             foreach (var str in args)
                 Console.Write(str + ' ');
+            args.Distinct();
             if (args.Length < 3) Message.WriteError("There must be more than 3 arguments");
             else if (args.Length % 2 == 0) Message.WriteError("The number of arguments must be odd");
-            else if (IsCorrect(args)) ;
-            else WriteExamples();
+            else if (GameRules.IsCorrect(args)) ;
+            else GameRules.WriteExamples();
 
         }        
         
